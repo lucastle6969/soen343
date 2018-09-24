18f1d4380056df9cea865aec891c03d41e5b738f
lucastle6969 <lucastle69@hotmail.com>
2018-09-23 20:59:49 -0400

    removed system.py and associated calls

--- app.py
+++ app.py
@@ -9,15 +9,13 @@ app = Flask(__name__)
 
 appdb = db(app)
 
-ourSystem = System('version1')
-
 @app.route('/')
 def index():
-    return render_template('home.html', sysObj = ourSystem)
+    return render_template('home.html')
 
 @app.route('/home')
 def home():
-    return render_template('home.html', sysObj = ourSystem)
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
--- domain/system.py
+++
@@ -1,6 +0,0 @@
-class System:
-    def __init__(self, name):
-        self.name = name
-
-    def welcome(self):
-        return "Welcome to the System I am " + self.name
