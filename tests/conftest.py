ecb6875acbd1333e26b3ce04d44e26c92dea80e0
PetiaPav <petia2397@yahoo.ca>
2018-09-28 01:37:18 -0400

    Addition of testing code and necessary files for future implementation

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+from model.User import User
+
+#@pytest.fixture(scope='module')
+#def new_user_creation():
+#    user = User('4134','john', 'doe', 'Sunset Avenue', 'johnDoe@gmail.com','5142234443',0,'FairytaleGoneBad')
+#    return user
