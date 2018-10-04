940ba20e21c95f85611d04207afe685809ae4091
PetiaPav <petia2397@yahoo.ca>
2018-10-03 20:07:51 -0400

    Working test for User class and Conftest initial setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,17 @@
-import pytest
-from model.User import User
+# file contains fixture functions to be used for multiple test files.
+
+#import os
+#import pytest
+
+#from flaskext.mysql import MySQL
+#from model import Tdg
+#from model import User
+
+#pytest_plugins = ['pytest_virtualenv']
 
 #@pytest.fixture(scope='module')
 #def new_user_creation():
 #    user = User('4134','john', 'doe', 'Sunset Avenue', 'johnDoe@gmail.com','5142234443',0,'FairytaleGoneBad')
 #    return user
+
+#@pytest.fixture(scope='session')
--- tests/unit/test_user.py
+++ tests/unit/test_user.py
@@ -1,8 +1,8 @@
 #from file import class
 from model.User import User
 
-#Functions need to start with test_
-def test_new_user(self):
+#Note: Functions need to start with test_
+def test_new_user():
     user = User('23452','John','Doe','Sunset Avenue', 'johndoe@gmail.com','5142235523', 0, 'FoundationTrilogy')
     user.id = '23452'
     user.firstname = 'John'
@@ -12,5 +12,5 @@ def test_new_user(self):
     user.phone = '5142235523'
     user.admin = 0
     user.password = 'FoundationTrilogy'
-    self.assertEqual(user, user) #Check for expected value
+    assert(user == user) #Check for expected value
 
