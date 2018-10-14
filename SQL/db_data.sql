159e6c05f6099d23922811c83b7d7617137ea94d
Michael-Gagnon <m.gagnon04@hotmail.com>
2018-10-14 16:15:26 -0400

    Changed the delete statement to truncate to make the auto-increments reset [#45]

--- SQL/db_data.sql
+++ SQL/db_data.sql
@@ -1,20 +1,20 @@
-DELETE FROM clientAdmin;
+TRUNCATE TABLE clientAdmin;
 INSERT INTO clientAdmin VALUES (1, 'John', 'Doe', '125 Ave Du Parc', 'johndoe@gmail.com', '514-336-4545', 1, '$5$rounds=535000$Y9aJsj6nDUIxrdX8$KjzneFoNwSBd8MiedeoCuhDR3AnFTQeYm7vBrCV/9a4');
 
 
-DELETE FROM book;
+TRUNCATE TABLE book;
 INSERT INTO book VALUES (NULL, 'A Feast For Crows', 'George RR Martin', 'Physical', 250, 'Scholastic', 'English', 1234567890, 1234567890123);
 
 
-DELETE FROM magazine;
+TRUNCATE TABLE magazine;
 INSERT INTO magazine VALUES (NULL, 'The New York Times Magazine', 'Arthurr Ochs Sulzberger Jr.', 'English', 1439401890, 8461968342412);
 
 
-DELETE FROM movie;
+TRUNCATE TABLE movie;
 INSERT INTO movie VALUES (NULL, 'The Last Jedi', 'Rian Johnson', 'Kathleen Kennedy', 'Mark Hamil, Carrie Fisher, Adam Driver and Daisy Ridley', 'English', 'None', 'None', 'Dec. 15 2017', '152 minutes');
 
 
-DELETE FROM music;
+TRUNCATE TABLE music;
 INSERT INTO music VALUES (NULL, 'CD', 'Thriller', 'Michael Jackson', 'Epic Records', 'Nov. 30 1982', ' B008FOB124');
 
 
