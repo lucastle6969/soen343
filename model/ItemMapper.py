ac22929ce7c57f1a304ae65edc291b3a7e2214bc
Mstc08 <minjun_TC@hotmail.com>
2018-10-20 14:34:32 -0400

    Sending lists instead of single items in end function

--- model/ItemMapper.py
+++ model/ItemMapper.py
@@ -9,8 +9,9 @@ class ItemMapper:
         self.uow = None
         self.catalog = Catalog()
         self.tdg = Tdg(app)
-        self.catalog.populate(self.tdg.get_books(), self.tdg.get_magazines(), self.tdg.get_movies(), self.tdg.get_music())
-    
+        self.catalog.populate(self.tdg.get_books(), self.tdg.get_magazines(),
+                              self.tdg.get_movies(), self.tdg.get_music())
+
     def get_catalog(self):
         return self.catalog
 
@@ -31,7 +32,8 @@ class ItemMapper:
         language = form.language.data
         isbn10 = form.isbn10.data
         isbn13 = form.isbn13.data
-        book = Book(None, title, prefix, status, author, book_format, pages, publisher, language, isbn10, isbn13)
+        book = Book(None, title, prefix, status, author, book_format, pages,
+                    publisher, language, isbn10, isbn13)
         if self.uow is None:
             self.uow = Uow()
         self.uow.add(book)
@@ -45,7 +47,8 @@ class ItemMapper:
         language = form.language.data
         isbn10 = form.isbn10.data
         isbn13 = form.isbn13.data
-        magazine = Magazine(None, title, prefix, status, publisher, language, isbn10, isbn13)
+        magazine = Magazine(None, title, prefix, status, publisher, language,
+                            isbn10, isbn13)
         if self.uow is None:
             self.uow = Uow()
         self.uow.add(magazine)
@@ -63,8 +66,8 @@ class ItemMapper:
         dubbed = form.dubbed.data
         release_date = form.releaseDate.data
         run_time = form.runtime.data
-        movie = Movie(None, title, prefix, status, director, producers, actors, language, subtitles, dubbed,
-                        release_date, run_time)
+        movie = Movie(None, title, prefix, status, director, producers, actors
+                      language, subtitles, dubbed, release_date, run_time)
         if self.uow is None:
             self.uow = Uow()
         self.uow.add(movie)
@@ -79,15 +82,26 @@ class ItemMapper:
         label = form.label.data
         release_date = form.releaseDate.data
         asin = form.asin.data
-        music = Music(None, title, prefix, status, media_type, artist, label, release_date, asin)
+        music = Music(None, title, prefix, status, media_type, artist, label,
+                      release_date, asin)
         if self.uow is None:
             self.uow = Uow()
         self.uow.add(music)
         self.uow.registerNew(music)
 
     def end(self):
-        items_to_commit = self.uow.commit()
+        items_to_commit = self.uow.get_saved_changes()
         self.uow = None
+
+        modified_books = []
+        modified_magazines = []
+        modified_movies = []
+        modified_music = []
+
+        deleted_books = []
+        deleted_magazines = []
+        deleted_movies = []
+        deleted_music = []
 
         # Add
         if items_to_commit[0] is not None:
@@ -108,39 +122,37 @@ class ItemMapper:
                     item_id = self.tdg.add_music(item)
                     item.id = item_id
                     self.catalog.add_item(item)
-                    
+
         # Modify
-        elif items_to_commit[1] is not None:
-            for item in items_to_commit[0]:
+        if items_to_commit[1] is not None:
+            for item in items_to_commit[1]:
                 if item.prefix == "bb":
-                    itemId = self.tdg.modify_book(item)
-                    # How is this going to work? there is no form to send..
-                    self.catalog.edit_item(itemId, form)
+                    modified_books.append(item)
                 elif item.prefix == "ma":
-                    itemId = self.tdg.modify_magazine(item)
-                    self.catalog.edit_item(itemId, form)
+                    modified_magazines.append(item)
                 elif item.prefix == "mo":
-                    itemId = self.tdg.modify_movie(item)
-                    self.catalog.edit_item(itemId, form)
+                    modified_movies.append(item)
                 elif item.prefix == "mu":
-                    itemId = self.tdg.modify_music(item)
-                    self.catalog.edit_item(itemId, form)
+                    modified_music.append(item)
+            self.catalog.edit_items(items_to_commit[1])
+            self.tdg.modify_books(modified_books)
+            self.tdg.modify_magazines(modified_magazines)
+            self.tdg.modify_movies(modified_movies)
+            self.tdg.modify_music(modified_music)
 
         # Delete
-        elif items_to_commit[2] is not None:
-            for item in items_to_commit[0]:
+        if items_to_commit[2] is not None:
+            for item in items_to_commit[2]:
                 if item.prefix == "bb":
-                    item_id = self.tdg.delete_book(item)
-                    self.catalog.delete_item(item_id)
+                    deleted_books.append(item)
                 elif item.prefix == "ma":
-                    item_id = self.tdg.delete_magazine(item)
-                    self.catalog.delete_item(item_id)
+                    deleted_magazines.append(item)
                 elif item.prefix == "mo":
-                    item_id = self.tdg.delete_movie(item)
-                    self.catalog.delete_item(item_id)
+                    deleted_movies.append(item)
                 elif item.prefix == "mu":
-                    item_id = self.tdg.delete_music(item)
-                    self.catalog.delete_item(item_id)
-
-        # self.catalog.update(itemsToCommit)
-        # self.tdg.update(itemsToCommit)
+                    deleted_music.append(item)
+            self.catalog.delete_items(items_to_commit[2])
+            self.tdg.delete_books(deleted_books)
+            self.tdg.delete_magazines(deleted_magazines)
+            self.tdg.delete_movies(deleted_movies)
+            self.tdg.delete_music(deleted_music)
