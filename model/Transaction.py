f2c4a15018ee24c214623f9c267ab00bc5dd0596
PetiaPav <petia2397@yahoo.ca>
2018-11-15 10:00:12 -0500

    adding to transaction related classes

--- model/Transaction.py
+++ model/Transaction.py
@@ -1,9 +1,9 @@
 
 class Transaction:
-    def __init__(self):
+    def __init__(self, transaction_id, user_fk, prefix, physical_id, transaction_type, timestamp):
         pass
 
 
 class ActiveLoan:
-    def __init__(self):
+    def __init__(self, ):
         pass
