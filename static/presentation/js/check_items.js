4321af3b2ae5b22b5001cab03fd71d959951be49
Vineet <koppalkarvineet@gmail.com>
2018-11-16 21:25:47 -0500

    [#122] Renamed return_items.js to check_items.js to stay consistent with loan

--- static/presentation/js/check_items.js
+++ static/presentation/js/check_items.js
@@ -7,7 +7,6 @@ $(function() {
             button.attr('class', 'form-control btn btn-primary float-right active')
             button.removeAttr('disabled')
         } else {
-            <!--$('#sub').attr('disabled', 'disabled');-->
             button.attr('class', 'form-control btn btn-primary float-right disabled')
             button.attr('disabled', 'disabled')
         }
