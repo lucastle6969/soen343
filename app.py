91a34ee35f5348d78ef53878a770b8cf65200cb0
PetiaPav <petia2397@yahoo.ca>
2018-11-17 10:11:06 -0500

    fix and added ajax script

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def add_to_cart(item_prefix, item_id):
         return redirect('/home')
     valid_cart_size = user_mapper.validate_cart_size(user_id)
     if valid_cart_size:
-        available_copy = item_mapper.get_available_copy(item_prefix, item_id)
+        available_copy = item_mapper.get_available_copy(item_prefix, int(item_id))
         if available_copy is not None:
             user_mapper.add_to_cart(user_id, available_copy)
             response = "added"
