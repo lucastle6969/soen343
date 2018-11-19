1cfb57623f5f9f75ea70c28b2034f3af44eb80ba
dfeokti <d_feokti@live.concordia.ca>
2018-11-19 13:03:37 -0500

    fixed test db sql file

--- SQL/db_test.sql
+++ SQL/db_test.sql
@@ -1,12 +1,12 @@
 INSERT INTO book_physical (item_fk, status, return_date, user_fk)
-VALUES (2, "Loaned", (DATE_ADD(now() , INTERVAL 7 DAY)), 3);
+VALUES (2, "Loaned", (DATE_ADD(now() , INTERVAL 7 DAY)), 5);
 INSERT INTO book_physical (item_fk, status, return_date, user_fk)
-VALUES (1, "Loaned", (DATE_ADD(now() , INTERVAL 5 DAY)), 3);
+VALUES (1, "Loaned", (DATE_ADD(now() , INTERVAL 5 DAY)), 5);
 INSERT INTO movie_physical (item_fk, status, return_date, user_fk)
-VALUES (2, "Loaned", (DATE_ADD(now() , INTERVAL 2 DAY)), 3);
+VALUES (2, "Loaned", (DATE_ADD(now() , INTERVAL 2 DAY)), 5);
 INSERT INTO movie_physical (item_fk, status, return_date, user_fk)
-VALUES (1, "Loaned", (DATE_ADD(now() , INTERVAL 1 DAY)), 3);
+VALUES (1, "Loaned", (DATE_ADD(now() , INTERVAL 1 DAY)), 5);
 INSERT INTO music_physical (item_fk, status, return_date, user_fk)
-VALUES (2, "Loaned", (DATE_ADD(now() , INTERVAL 2 DAY)), 3);
+VALUES (2, "Loaned", (DATE_ADD(now() , INTERVAL 2 DAY)), 5);
 INSERT INTO music_physical (item_fk, status, return_date, user_fk)
-VALUES (1, "Loaned", (DATE_ADD(now() , INTERVAL 1 DAY)), 3);
+VALUES (1, "Loaned", (DATE_ADD(now() , INTERVAL 1 DAY)), 5);
